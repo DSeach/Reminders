6cf9dd0e2406a676566a9d3d55d94dee98c574ac
DSeach <djseach@gmail.com>
2024-12-31 19:06:16 +0800

    Added more information to dates, mostly proof of concept stuff, but documented the idea going forward. also make a makefile.

--- Dates/ReminderSystem.c
+++ Dates/ReminderSystem.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include "ReminderSystem.h"
+
+	/**
+	* @author David Seach
+	* @date 31/12/2024
+	* @brief
+	* A program to read a file and calculate how many days it is until 
+	* that date. 
+	*
+	* The Stuct I am using (stored in ReminderSystem.h) is called timeRecord
+	* and it used a struct tm that is found in time.h, I want to fill out
+	* the timeRecord->tm_mday and timeRecord->mon to calculate the total 
+	* seconds using localtime() also found in time.h to see if the date has
+	* passed and if it hasnt, how many days until it does. If the date has 
+	* passed then the char in timeRecord (tr_rep) will either add a year 
+	* for next year's date or remove the entry in the file.
+	*
+	* file name for data: dates
+	*
+	**/
 
 int main(int argc , char* argv[]){
 	time_t myTime = time(NULL);
-
+	struct timeRecord records[10];
+	const char* fileName = "dates";
 	struct tm* currentTime = localtime(&myTime);
 	
 	struct tm christmas = {0};
@@ -15,6 +37,29 @@ int main(int argc , char* argv[]){
 	if(myTime > chrissyTime){printf("Christmas has already happend!\n");}
 	else{printf("Christmas is %d days away!\n" , (chrissyTime - myTime) / 60 / 60 / 24);}
 
-	//printf("%s\n" , timeToPrint);
+	FILE* fp = fopen(fileName , "r+" );
+
+	if(fp == NULL){
+		perror("Error opening file\n");
+		exit(-1);
+	}
+
+	FillRecords(records , fp);
+
+	fclose(fp);
 	return 0;
 }
+
+void FillRecords(struct timeRecord* records , FILE* fd){
+	char* line = NULL;
+	ssize_t nread;
+	size_t len = 0;
+
+	while((nread = getline(&line , &len , fd)) != -1){
+	//	printf("Reading ... %s \n" , line);
+		fwrite(line,nread,1,stdout);
+	}
+	free(line);
+}
+
+
---
+++ Dates/ReminderSystem.h
@@ -0,0 +1,14 @@
+#ifndef REMINDERSYSTEM_H
+#define REMINDERSYSTEM_H
+
+#include <time.h>
+
+struct timeRecord {
+	struct tm* tr_tm;
+	char* tr_name;
+	char* tr_rep;
+};
+
+void FillRecords(struct timeRecord* records , FILE* fd);
+
+#endif
